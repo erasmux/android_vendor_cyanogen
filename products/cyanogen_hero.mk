1f8f045da6ac23623d4df12934ef19a32358d1c3
Eran Mizrahi <eranmizra@gmail.com>
2011-06-13 02:45:06 +0300

    Floyo 1.4pre1

--- products/cyanogen_hero.mk
+++ products/cyanogen_hero.mk
@@ -31,7 +31,7 @@ WITH_WINDOWS_MEDIA := true
 # Set ro.modversion
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.modversion=FloyoCM-1.2.0
+	ro.modversion=FloyoCM-1.4pre1
 
 #
 # Copy passion specific prebuilt files
